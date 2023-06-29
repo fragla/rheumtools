190645637d72f82e149f30beff2297541dbc4a80
fragla <fraser.morton@glasgow.ac.uk>
2023-06-29 13:23:05 +0100

    Additional BMI tests added.

--- tests/testthat/test-bmi.R
+++ tests/testthat/test-bmi.R
@@ -1,11 +1,14 @@
 test_that("BMI gives correct answer", {
   expect_equal(body_mass_index(height = 1.8, weight = 85), 26.2)
+  expect_equal(body_mass_index(height = 1.8, weight = NA), NA_real_)
+  expect_equal(body_mass_index(height = NA, weight = 85), NA_real_)
   expect_equal(body_mass_index(height = c(1.75, 1.52), weight = c(78, 55.2)), c(25.5, 23.9))
 })
 
 test_that("BMI throws error for incorrect parameters", {
   expect_error(body_mass_index(height = 1.8, weight = NA, ignore = FALSE))
   expect_error(body_mass_index(height = NA, weight = 73.2, ignore = FALSE))
+  expect_error(body_mass_index(height = 1.75, weight = c(78, 55.2)))
 })
 
 test_that("BMI warns for suspected height in centimetres", {
