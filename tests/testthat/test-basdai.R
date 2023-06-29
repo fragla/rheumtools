2d57e23c22e9afcf3a5bfcd611eb201b8c921b73
fragla <fraser.morton@glasgow.ac.uk>
2023-06-29 13:19:36 +0100

    Additional BASDAI tests added.

--- tests/testthat/test-basdai.R
+++ tests/testthat/test-basdai.R
@@ -3,6 +3,10 @@ test_that("BASDAI gives correct answer", {
   expect_equal(basdai_score(1:2, 2:3, 3:4, 4:5, 5:6, 6:7), c(3.1, 4.1))
   expect_equal(basdai_score(0, 0, 3, 4, 5, 0), 1.9)
   expect_equal(basdai_score(NA, 2, 3, 4, 5, 10), NA_real_)
+  expect_equal(basdai_score(1, 21, 3, 4, 5, 10), NA_real_)
+  expect_equal(basdai_score(1, 2, NA, 4, 5, 10), NA_real_)
+  expect_equal(basdai_score(1, 2, 3, "B", 5, 10), NA_real_)
+  expect_equal(basdai_score(1, 2, 3, 5, NA, 10), NA_real_)
   expect_equal(basdai_score(4, 2, 3, 4, 5, 12), NA_real_)
 })
 
