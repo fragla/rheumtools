657c61d1bbe297c5393e6da2d970f71bae13a3bd
fragla <fraser.morton@glasgow.ac.uk>
2023-06-29 12:57:15 +0100

    Additional DLQI tests added.

--- tests/testthat/test-dlqi.R
+++ tests/testthat/test-dlqi.R
@@ -1,9 +1,35 @@
 test_that("DLQI score gives correct answer", {
-  expect_equal(dlqi_score("Very much", "A lot", "A little","Not at all", "Not relevant", "A lot", "Yes", "Very much", "A lot", "A little"), 17)
-  expect_equal(dlqi_score("Very much", "A lot", "A little","Not at all", "Not relevant", "A lot", "No", "Very much", "A lot", "A little"), NA_real_)
-  expect_equal(dlqi_score("Very much", "A lot", "A little","Not at all", "Not relevant", "A lot", "Very much", "Very much", "A lot", "A little"), NA_real_)
-  expect_equal(dlqi_score("Very much", "A lot", "A little","Not at all", "Not relevant", "A lot", "A lot", "Very much", NA, "A little"), NA_real_)
-  expect_equal(dlqi_score("Very much", "A lot", "A little","Not at all", "Not relevant", "A lot", "A lot", "Very much", "A lot", "A little"), 16)
+  expect_equal(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", "A lot", "Yes", "Very much", "A lot", "A little"), 17)
+  expect_equal(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", "A lot", "No", "Very much", "A lot", "A little"), NA_real_)
+  expect_equal(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", "A lot", "Very much", "Very much", "A lot", "A little"), NA_real_)
+  expect_equal(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", "A lot", "A lot", "Very much", NA, "A little"), NA_real_)
+  expect_equal(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", "A lot", "A lot", "Very much", "A lot", "A little"), 16)
+  expect_equal(dlqi_score(NA, "A lot", "A little","Not at all", "Not relevant", "A lot", "A lot", "Very much", "A lot", "A little"), NA_real_)
+  expect_equal(dlqi_score("Very much", NA, "A little", "Not at all", "Not relevant", "A lot", "A lot", "Very much", "A lot", "A little"), NA_real_)
+  expect_equal(dlqi_score("Very much", "A lot", NA, "Not at all", "Not relevant", "A lot", "A lot", "Very much", "A lot", "A little"), NA_real_)
+  expect_equal(dlqi_score("Very much", "A lot", "A little", NA, "Not relevant", "A lot", "A lot", "Very much", "A lot", "A little"), NA_real_)
+  expect_equal(dlqi_score("Very much", "A lot", "A little", "Not at all", NA, "A lot", "A lot", "Very much", "A lot", "A little"), NA_real_)
+  expect_equal(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", NA, "A lot", "Very much", "A lot", "A little"), NA_real_)
+  expect_equal(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", "A lot", NA, "Very much", "A lot", "A little"), NA_real_)
+  expect_equal(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", "A lot", "A lot", NA, "A lot", "A little"), NA_real_)
+  expect_equal(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", "A lot", "A lot", "Very much", NA, "A little"), NA_real_)
+  expect_equal(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", "A lot", "A lot", "Very much", "A lot", NA), NA_real_)
+})
+
+test_that("DLQI score throws error for incorrect parameters", {
+  expect_error(dlqi_score(NA, "A lot", "A little","Not at all", "Not relevant", "A lot", "A lot", "Very much", "A lot", "A little", ignore = FALSE))
+  expect_error(dlqi_score("Very much", NA, "A little", "Not at all", "Not relevant", "A lot", "A lot", "Very much", "A lot", "A little", ignore = FALSE))
+  expect_error(dlqi_score("Very much", "A lot", NA, "Not at all", "Not relevant", "A lot", "A lot", "Very much", "A lot", "A little", ignore = FALSE))
+  expect_error(dlqi_score("Very much", "A lot", "A little", NA, "Not relevant", "A lot", "A lot", "Very much", "A lot", "A little", ignore = FALSE))
+  expect_error(dlqi_score("Very much", "A lot", "A little", "Not at all", NA, "A lot", "A lot", "Very much", "A lot", "A little", ignore = FALSE))
+  expect_error(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", NA, "A lot", "Very much", "A lot", "A little", ignore = FALSE))
+  expect_error(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", "A lot", NA, "Very much", "A lot", "A little", ignore = FALSE))
+  expect_error(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", "A lot", "A lot", NA, "A lot", "A little", ignore = FALSE))
+  expect_error(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", "A lot", "A lot", "Very much", NA, "A little", ignore = FALSE))
+  expect_error(dlqi_score("Very much", "A lot", "A little", "Not at all", "Not relevant", "A lot", "A lot", "Very much", "A lot", NA, ignore = FALSE))
+  expect_error(dlqi_score(q1=c("Very much","Very much"), q2=c("A lot","Very much"), q3=c("A little","Very much"), q4=c("Not at all","Very much"), q5=c("Not relevant","Very much"),
+                          q6=c("A lot","Very much"), q7="Yes", q8=c("Very much","Very much"), q9=c("A lot","Very much"), q10=c("A little","Very much")))
+
 })
 
 test_that("DLQI classification gives correct answer", {
